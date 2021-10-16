0bf2c2c8378ab6dd1441c2609b50c92fd7b42482
Yen <wai.imyen@gmail.com>
2021-10-16 22:36:40 +0800

    update: set default value to competitor table

--- database/migrations/2021_10_14_084015_create_competitors_table.php
+++ database/migrations/2021_10_14_084015_create_competitors_table.php
@@ -15,13 +15,13 @@ class CreateCompetitorsTable extends Migration
     {
         Schema::create('competitors', function (Blueprint $table) {
             $table->id();
-            $table->string('urn_competitor')->comment('選手編號');
-            $table->string('name')->comment('名稱');
-            $table->string('name_zht')->comment('中文名稱');
-            $table->string('country')->comment('國家');
-            $table->string('gender')->comment('性別');
-            $table->string('rank')->comment('排名');
-            $table->string('image', 1024)->comment('選手圖');
+            $table->string('urn_competitor')->default('')->comment('選手編號');
+            $table->string('name')->default('')->comment('名稱');
+            $table->string('name_zht')->default('')->comment('中文名稱');
+            $table->string('country')->default('')->comment('國家');
+            $table->string('gender')->default('')->comment('性別');
+            $table->string('rank')->default('')->comment('排名');
+            $table->string('image', 1024)->default('')->comment('選手圖');
             $table->timestamps();
         });
     }
