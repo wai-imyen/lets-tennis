efac3af58203dfc1da14290c9bb03fa484fac223
Yen <wai.imyen@gmail.com>
2021-10-17 22:21:33 +0800

    update: adjust competitor table rank field

--- database/migrations/2021_10_14_084015_create_competitors_table.php
+++ database/migrations/2021_10_14_084015_create_competitors_table.php
@@ -20,7 +20,7 @@ class CreateCompetitorsTable extends Migration
             $table->string('name_zht')->default('')->comment('中文名稱');
             $table->string('country')->default('')->comment('國家');
             $table->string('gender')->default('')->comment('性別');
-            $table->string('rank')->default('')->comment('排名');
+            $table->bigInteger('rank')->default(0)->comment('排名');
             $table->string('image', 1024)->default('')->comment('選手圖');
             $table->timestamps();
         });
